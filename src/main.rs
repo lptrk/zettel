12bfcdbbcbc00f8efedee1b44b978dfc74e9b0d6
lptrk <lktpriv@gmail.com>
2024-02-29 08:28:11 +0100

    Hello, Zettel!

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,12 @@ impl Render for HelloWorld {
     fn render(&mut self, _cx: &mut ViewContext<Self>) -> impl IntoElement {
         div()
             .flex()
-            .bg(rgb(0x2e7d32))
+            .bg(rgb(0xffffff))
             .size_full()
             .justify_center()
             .items_center()
             .text_xl()
-            .text_color(rgb(0xffffff))
+            .text_color(rgb(0x000000))
             .child(format!("Hello, {}!", &self.text))
     }
 }
@@ -22,7 +22,7 @@ fn main() {
     App::new().run(|cx: &mut AppContext| {
         cx.open_window(WindowOptions::default(), |cx| {
             cx.new_view(|_cx| HelloWorld {
-                text: "World".into(),
+                text: "Zettel".into(),
             })
         });
     });
