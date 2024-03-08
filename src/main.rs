fca7e1e3d29d558e7246d741caabfd6ee558e989
lptrk <lktpriv@gmail.com>
2024-03-08 12:17:13 +0100

    Hello, zettel

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,28 @@
-use gpui::*;
+use iced::{widget::text, Element, Sandbox, Settings};
 
-struct HelloWorld {
-    text: SharedString,
+fn main() -> iced::Result {
+    Editor::run(Settings::default())
 }
 
-impl Render for HelloWorld {
-    fn render(&mut self, _cx: &mut ViewContext<Self>) -> impl IntoElement {
-        div()
-            .flex()
-            .bg(rgb(0xffffff))
-            .size_full()
-            .justify_center()
-            .items_center()
-            .text_xl()
-            .text_color(rgb(0x000000))
-            .child(format!("Hello, {}!", &self.text))
+struct Editor;
+
+#[derive(Debug)]
+enum Message {}
+impl Sandbox for Editor {
+    type Message = Message;
+
+    fn new() -> Self {
+        Self
     }
-}
+    fn title(&self) -> String {
+        String::from("zettel")
+    }
 
-fn main() {
-    App::new().run(|cx: &mut AppContext| {
-        cx.open_window(WindowOptions::default(), |cx| {
-            cx.new_view(|_cx| HelloWorld {
-                text: "Zettel".into(),
-            })
-        });
-    });
+    fn update(&mut self, message: Self::Message) {
+        match message {}
+    }
+
+    fn view(&self) -> iced::Element<'_, Self::Message> {
+        text("Hello, zettel").into()
+    }
 }
