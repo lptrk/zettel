cc5def23fb998b11f8b101e75fa8bc0dd674cf07
lptrk <lktpriv@gmail.com>
2024-03-08 12:37:49 +0100

    added readme piucture

---
+++ README.md
@@ -0,0 +1,3 @@
+# zettel
+
+![image](https://github.com/lptrk/zettel/assets/96191959/70839c51-21cf-4a4d-9ce7-19d44dc76a6b)
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,41 @@
-use iced::{widget::text, Element, Sandbox, Settings};
+use iced::{
+    widget::{text, text_editor},
+    Element, Sandbox, Settings,
+};
 
 fn main() -> iced::Result {
     Editor::run(Settings::default())
 }
 
-struct Editor;
+struct Editor {
+    content: text_editor::Content,
+}
 
-#[derive(Debug)]
-enum Message {}
+#[derive(Debug, Clone)]
+enum Message {
+    Edit(text_editor::Action),
+}
 impl Sandbox for Editor {
     type Message = Message;
 
     fn new() -> Self {
-        Self
+        Self {
+            content: text_editor::Content::new(),
+        }
     }
     fn title(&self) -> String {
         String::from("zettel")
     }
 
     fn update(&mut self, message: Self::Message) {
-        match message {}
+        match message {
+            Message::Edit(action) => {
+                self.content.edit(action);
+            }
+        }
     }
 
     fn view(&self) -> iced::Element<'_, Self::Message> {
-        text("Hello, zettel").into()
+        text_editor(&self.content).on_edit(Message::Edit).into()
     }
 }
